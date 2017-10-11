e71f00b2dee5fbf541f039cb7ca0fcd701547133
Guest User <Guest@epicodus-3.local>
2017-10-11 16:47:30 -0700

    add startup changes

--- TravelBlog/Startup.cs
+++ TravelBlog/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TravelBlog.Models;
 
 namespace TravelBlog
 {
